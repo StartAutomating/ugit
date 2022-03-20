c96cda6e66212620d43ff8c03a19cb7ebeae3d09
James Brundage <@github.com>
2022-03-20 02:52:29 -0700

    Updating Tests (adding test for git log)

--- ugit.tests.ps1
+++ ugit.tests.ps1
@@ -4,4 +4,12 @@ describe ugit {
     it "Updates git" {
         (Get-Command git) -is [Management.Automation.AliasInfo] | Should -be $true
     }
+
+    context 'git log' {
+        it 'Can get log entries as objects' {
+            $logEntry = git log -n 1 
+            $logEntry.commitHash | Should -not -be $null
+            $logEntry.CommitDate | Should -BeLessThan ([DateTime]::now)
+        }
+    }
 }
