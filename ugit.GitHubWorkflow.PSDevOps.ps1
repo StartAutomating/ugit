fdf63283e3920caa576d968c10c820a78557d404
James Brundage <@github.com>
2022-06-28 18:15:03 -0700

    Updating Workflow source file (#58 #59)

--- ugit.GitHubWorkflow.PSDevOps.ps1
+++ ugit.GitHubWorkflow.PSDevOps.ps1
@@ -1,13 +1,7 @@
 ﻿#requires -Module PSDevOps
 Push-Location $PSScriptRoot
 
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish |
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, UsePiecemeal, RunEZOut, HelpOut |
     Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
-
-New-GitHubWorkflow -Name "Run HelpOut" -On Push  -Job HelpOut |
-    Set-Content .\.github\workflows\UpdateDocs.yml -Encoding UTF8 -PassThru
-
-New-GitHubWorkflow -Name "Run EZOut" -On Push -Job RunEZOut |
-    Set-Content .\.github\workflows\RunEZOut.yml -Encoding UTF8 -PassThru
 
 Pop-Location
