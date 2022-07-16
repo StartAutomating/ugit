819e5165042e0e33ef9475a4102681b9c85017a9
James Brundage <@github.com>
2022-07-15 17:49:40 -0700

    Adding formatting source for git stash (re #65)

---
+++ Formatting/Git.Stash.Nothing.format.ps1
@@ -0,0 +1,5 @@
+Write-FormatView -TypeName git.stash.nothing -Action {    
+    Write-FormatViewExpression -Newline
+    Write-FormatViewExpression -Text "Nothing to stash" -ForegroundColor Verbose
+    Write-FormatViewExpression -Newline
+}
