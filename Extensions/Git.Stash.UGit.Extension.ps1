69186b805dd35848a9b35dab9b383b4ef2bf096d
James Brundage <@github.com>
2022-07-16 02:42:44 -0700

    Git stash extension: support for pop/apply (#65)

--- Extensions/Git.Stash.UGit.Extension.ps1
+++ Extensions/Git.Stash.UGit.Extension.ps1
@@ -14,6 +14,7 @@ param(
 
 begin {
     $stashLines = @()
+    $outputBase = [Ordered]@{GitRoot=$GitRoot;Gitcommand=$GitCommand}
 }
 
 process {
@@ -23,44 +24,94 @@ process {
 end {
     $stashOutputHandled = $false
     
-    
+    $stashLineNumber = 0
+    $gitStashOut     = [Ordered]@{
+        GitRoot = $GitRoot
+        GitCommand = $GitCommand
+        GitOutputLines = $stashLines
+    }
+    $inPhase     = ''
     foreach ($stashLine in $stashLines) {
+        $stashLineNumber++
         if ($stashLine -eq 'No local changes to save') {
-            return [PSCustomObject][Ordered]@{
-                PSTypeName = 'git.stash.nothing'
-                GitRoot = $GitRoot
-                GitCommand = $GitCommand
-                GitOutputLines = $stashLines
-            }
-            
+            return [PSCustomObject]([Ordered]@{
+                PSTypeName = 'git.stash.nothing'                
+            } + $gitStashOut)
         }
 
         if ($GitCommand -match '^git stash list') {
             if ($stashLine -match '^stash\@\{(?<Number>\d+)\}:\s{0,}(?<Message>.+$)') {
                 $stashOutputHandled = $true
-                [PSCustomObject][Ordered]@{
+                [PSCustomObject]([Ordered]@{
                     PSTypeName = 'git.stash.entry'
                     Number  = [int]$matches.Number
-                    Message = $matches.Message
-                    GitRoot = $GitRoot
-                    GitCommand = $GitCommand
-                    GitOutputLines = $stashLines
-                }
+                    Message = $matches.Message                    
+                } + $gitStashOut)
             }
         }
 
         if ($GitCommand -eq 'git stash' -and 
             $stashLine -match 'Saved working directory and index state\s{0,}(?<Message>.+$)') {
             $stashOutputHandled = $true
-            [PSCustomObject][Ordered]@{
+            [PSCustomObject]([Ordered]@{
                 PSTypeName = 'git.stash.entry'
                 Number  = [int]0
                 Message = $matches.Message
-                GitRoot = $GitRoot
-                GitCommand = $GitCommand
-                GitOutputLines = $stashLines
-            }                        
+            } + $gitStashOut)                                     
         }
+
+        if ($GitCommand -match '^git stash (?>pop|apply)') {
+            if ($stashLineNumber -eq 1) {
+                $gitStashOut.BranchName = @($stashLine -split ' ' -ne '')[-1] 
+            }
+            if ($stashLine -like '*not staged for commit:*') {
+                # When on a new branch with no upstream, this comes first.
+                $inPhase = 'Unstaged'
+                continue
+            }
+            if ($stashLine -like "Changes to be committed:*") {
+                $inPhase = 'Staged'
+                continue
+            }
+            if ($stashLine -like "Untracked files:*") {
+                $inPhase = 'Untracked'
+                continue
+            }
+
+            if ($stashLineNumber -eq 2 -and -not $inPhase) {
+                $gitStashOut.Status = $stashLine
+                continue
+            }
+
+            if ($stashLine -match '^\s+\(') { continue }
+            if ($stashLine -match '^\s+' -and $inPhase) {
+                $trimmedLine = $stashLine.Trim()
+                $changeType = 
+                    if ( $trimmedLine -match "^([\w\s]+):") {
+                        $matches.1
+                    } else {
+                        ''
+                    }
+                $changePath = $trimmedLine -replace "^[\w\s]+:\s+"
+                if ($inPhase -eq 'untracked') {
+                    $gitStashOut.$inPhase += Get-Item -ErrorAction SilentlyContinue -Path $changePath
+                } else {
+                    $gitStashOut.$inPhase += [PSCustomObject]@{
+                        ChangeType = $changeType -replace '\s'
+                        Path       = $changePath
+                        File       = Get-Item -ErrorAction SilentlyContinue -Path $changePath
+                    }
+                }
+                
+            }
+        }
+    }
+
+    if ($GitCommand -match '^git stash (?<action>(?>apply|pop))') {
+        $stashOutputHandled = $true
+        [PSCustomObject]([Ordered]@{
+            PSTypeName = "git.stash.apply"
+        } + $gitStashOut)
     }
 
     if (-not $stashOutputHandled) {
