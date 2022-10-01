0c9121da8dd665d03b3a05876ba6a2859020236c
James Brundage <@github.com>
2022-10-01 15:47:27 -0700

    Updating ugit workflow (Fixes #76)

--- ugit.GitHubWorkflow.PSDevOps.ps1
+++ ugit.GitHubWorkflow.PSDevOps.ps1
@@ -1,7 +1,7 @@
 ﻿#requires -Module PSDevOps
 Push-Location $PSScriptRoot
-
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, UsePiecemeal, RunEZOut, HelpOut |
+Import-BuildStep -ModuleName ugit
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, buildugit |
     Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
 
 Pop-Location
