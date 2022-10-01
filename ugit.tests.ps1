3745fd6634cc1be13218ef0cce7000d49caec606
James Brundage <@github.com>
2022-10-01 15:52:47 -0700

    Adding test for git init and git branch

--- ugit.tests.ps1
+++ ugit.tests.ps1
@@ -5,6 +5,25 @@ describe ugit {
         (Get-Command git) -is [Management.Automation.AliasInfo] | Should -be $true
     }
 
+    context 'git branch' {
+        it 'Can list branches' {
+            git branch |
+                Select-Object -ExpandProperty BranchName |
+                Should -BeLike '*'
+        }
+    }
+    
+    context 'git init' {
+        it 'Can initialize repositories' {
+            $randomDirName = "d$(Get-Random)"
+            $null = New-Item -Path $randomDirName -ItemType Directory
+            Push-Location $randomDirName
+            git init | Select-Object -ExpandProperty GitRoot | Should -Match "$randomDirName[\\/]{0,}$"
+            Pop-Location
+            Remove-Item $randomDirName -Recurse -Force
+        }
+    }
+
     context 'git log' {
         it 'Can get log entries as objects' {
             $logEntry = git log -n 1 
