d0a49d4752d7c35cfed71984461efa29a035de5d
James Brundage <@github.com>
2022-10-29 22:57:37 -0700

    Adding git help extension (Fixes #88)

---
+++ Extensions/Git.Help.All.UGit.Extension.ps1
@@ -0,0 +1,40 @@
+<#
+.Synopsis
+    git help all
+.Description
+    Returns git help --all as objects.
+.Example
+    git help -a
+.EXAMPLE
+    git help --all
+#>
+[Management.Automation.Cmdlet("Out","Git")]
+[ValidatePattern('^git help -(?>a|-all)')]
+[OutputType('git.command')]
+param()
+
+begin {
+    $category = ''
+}
+
+process {
+    if ($gitOut -match '^\S' -and $gitOut -notmatch '^See') {
+        $category = "$gitOut"
+        return
+    }
+
+    if ($gitOut -match '^See') { return }
+    if ($gitOut -match '^\s{0,}$') { return }
+    $null, $name, $description = "$gitOut" -split "\s+", 3
+
+    [PSCustomObject][Ordered]@{
+        PSTypeName = 'git.command'
+        Name = $name
+        Description = $description
+        Category = $category
+    }
+}
+
+end {
+
+}
