3fce9a3b74e9ae6502fe149da606ae3e662c762e
James Brundage <@github.com>
2022-10-29 22:53:52 -0700

    git log extension:  Improving singlefile diff behavior (Fixes #87)

--- Types/git.log/Diff.ps1
+++ Types/git.log/Diff.ps1
@@ -1,3 +1,12 @@
 Push-Location $this.GitRoot
-git diff $this.CommitHash @args
+$logPaths = @($this.GitArgument -ne 'log' -notmatch '^\-')
+foreach ($logPath in $logPaths) {
+    if (Test-Path $logPath) {
+        $relativeArgs = @("--relative", $logPath)
+        git diff $this.CommitHash @relativeArgs @args    
+    }
+}
+if (-not $logPaths) {
+    git diff $this.CommitHash @args
+}
 Pop-Location
