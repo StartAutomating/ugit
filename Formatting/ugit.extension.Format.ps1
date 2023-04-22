1125355dd338da46ef78d9380417457f654b3365
James Brundage <@github.com>
2023-04-22 16:40:08 -0700

    Adding ugit.extension formatting (Fixes #164)

---
+++ Formatting/ugit.extension.Format.ps1
@@ -0,0 +1,3 @@
+Write-FormatView -TypeName ugit.extension -Property DisplayName, Synopsis -AutoSize
+
+Write-FormatView -TypeName ugit.extension -Property DisplayName, Pattern -Name Pattern -AutoSize
