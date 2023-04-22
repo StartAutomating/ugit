0d7cab6e975b0cf9076ea252a7e163d06d2bdabc
James Brundage <@github.com>
2023-04-21 18:03:34 -0700

    git log formatting - right-aligning CommitMessage (Fixes #152)

--- Formatting/Git.Log.format.ps1
+++ Formatting/Git.Log.format.ps1
@@ -1,5 +1,17 @@
-Write-FormatView -TypeName Git.Log -Property GitUserName, CommitDate, CommitMessage -Wrap
+Write-FormatView -TypeName Git.Log -Property GitUserName, CommitDate, CommitMessage -Wrap -AlignProperty @{
+    "CommitDate" = "Right"
+    "CommitMessage" = "Left"
+}
 
-Write-FormatView -TypeName Git.Log -Property GitUserName, CommitDate, CommitHash, CommitMessage -Wrap -Name IncludeCommitHash
+Write-FormatView -TypeName Git.Log -Property GitUserName, CommitDate, CommitHash, CommitMessage -Wrap -Name IncludeCommitHash  -AlignProperty @{
+    "CommitDate" = "Right"
+    "CommitHash" = "Left"
+    "CommitMessage" = "Left"
+}
 
-Write-FormatView -TypeName Git.Merge.Log -Property GitUserName, CommitDate, MergeHash, CommitMessage -Wrap 
+Write-FormatView -TypeName Git.Merge.Log -Property GitUserName, CommitDate, MergeHash, CommitMessage -Wrap   -AlignProperty @{
+    "CommitDate" = "Right"
+    "CommitHash" = "Left"
+    "MergeHash"  = "Left"
+    "CommitMessage" = "Left"
+}
