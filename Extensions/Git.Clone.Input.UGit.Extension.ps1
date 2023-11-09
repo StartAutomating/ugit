8ad0976b89270ed2e9530d5070744439c007352c
James Brundage <@github.com>
2023-11-08 21:14:17 -0800

    enhancement: git clone -NoCheckout (Fixes #221)

--- Extensions/Git.Clone.Input.UGit.Extension.ps1
+++ Extensions/Git.Clone.Input.UGit.Extension.ps1
@@ -7,13 +7,27 @@
     By default, if --progress is not found, it will be added to any git clone.
 .EXAMPLE
     git clone https://github.com/MDN/content.git # This is a big repo.  Progress bars will be very welcome.
+.EXAMPLE
+    # If we don't check things out, cloning is faster.
+    git clone https://github.com/PowerShell/PowerShell -NoCheckout 
+    # (of course, that's because we're not copying files, just history)
 #>
 [ValidatePattern('^git clone')]
 [Management.Automation.Cmdlet("Use","Git")]
 [CmdletBinding(PositionalBinding=$false)]
 param(
+# If set, will not check out files from the respository.
+[Parameter(ValueFromPipelineByPropertyName)]
+[switch]
+$NoCheckout
 )
 
-if ($gitArgument -notcontains '--progress') {
+if ($NoCheckout) {
+    '--no-checkout'
+}
+
+if (
+    $gitArgument -notcontains '--progress'
+) {
     '--progress'
 }
