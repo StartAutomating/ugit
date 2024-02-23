06ff4d5af15a85d69539caface3febf9dbe582ec
James Brundage <@github.com>
2024-02-23 14:18:00 -0800

    fix: git.output.NotGitCommand ( Fixes #236 )
    Adding docs, fixing whitespace in pattern.

--- Types/git.output/get_NotGitCommand.ps1
+++ Types/git.output/get_NotGitCommand.ps1
@@ -1,3 +1,9 @@
-if ($this -match "^git:\s'(?<cmd>.+?)'is not a git command.") {
+<#
+.SYNOPSIS
+    Gets what was not a Git Command
+.DESCRIPTION
+    If the git output is not a git command, this will output the command name.
+#>
+if ($this -match "^git:\s'(?<cmd>.+?)' is not a git command.") {
     return $matches.cmd
 }
