e59c38c2f487b495dcd6d23a4d8201f1ec2d4dc2
James Brundage <@github.com>
2024-02-28 13:20:37 -0800

    feat: Mounting My ugit ( Fixes #246 )

--- ugit.ps.psm1
+++ ugit.ps.psm1
@@ -5,6 +5,15 @@ $MyModule = $MyInvocation.MyCommand.ScriptBlock.Module
 $MyModule.pstypenames.insert(0,$MyModule.Name)
 $ExecutionContext.SessionState.PSVariable.Set($MyModule.Name, $myModule)
 
-New-PSDrive -Name $MyModule.Name -PSProvider FileSystem -ErrorAction Ignore -Scope Global -Root ($MyModule | Split-Path)
+$newDriveSplat = @{PSProvider='FileSystem';ErrorAction='Ignore';Scope='Global'}
+New-PSDrive -Name $MyModule.Name -Root ($MyModule | Split-Path) @newDriveSplat
+
+if ($home) {
+    $myMyModule = "My$($myModule.Name)"
+    $myMyModuleRoot = Join-Path $home $myMyModule
+    if (Test-Path $myMyModuleRoot) {
+        New-PSDrive -Name $myMyModule -Root $myMyModuleRoot @newDriveSplat
+    }
+}
 
 Export-ModuleMember -Function * -Alias * -Variable $MyModule.Name
