55972dfa889ac1bb74f97d39c0f43f48a3e7f1eb
James Brundage <@github.com>
2024-02-28 11:34:56 -0800

    feat: Exporting Module as Variable $ugit ( Fixes #247 )

--- ugit.ps.psm1
+++ ugit.ps.psm1
@@ -1,2 +1,8 @@
 $CommandsPath = Join-Path $PSScriptRoot Commands
 [include('*-*.ps1')]$CommandsPath
+
+$MyModule = $MyInvocation.MyCommand.ScriptBlock.Module
+$MyModule.pstypenames.insert(0,$MyModule.Name)
+$ExecutionContext.SessionState.PSVariable.Set($MyModule.Name, $myModule)
+
+Export-ModuleMember -Function * -Alias * -Variable $MyModule.Name
