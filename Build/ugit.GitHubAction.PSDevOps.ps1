2780ce063892ee401b6ef394117fe1f1404f8968
James Brundage <@github.com>
2024-02-28 14:19:46 -0800

    chore: GitHub Action Definition - Updating Pathing ( Fixes #241 )

--- Build/ugit.GitHubAction.PSDevOps.ps1
+++ Build/ugit.GitHubAction.PSDevOps.ps1
@@ -2,4 +2,6 @@
 Import-BuildStep -SourcePath (
     Join-Path $PSScriptRoot 'GitHub'
 ) -BuildSystem GitHubAction
+Push-Location ($PSScriptRoot | Split-Path)
 New-GitHubAction -Name "UseUGit" -Description 'Updated Git' -Action UGitAction -Icon git-merge -OutputPath .\action.yml
+Pop-Location
