e2bbc29b1d5e435d9f51ec88d7535f8f4f4c15a5
StartAutomating <StartAutomating@github.com>
2024-02-28 19:05:03 +0000

    feat: Compiling ugit .psm1 ( Fixes #245 )

--- ugit.psm1
+++ ugit.psm1
@@ -1,4 +1,11 @@
-﻿$commandsPath = Join-Path $PSScriptRoot Commands
-. $commandsPath\Get-UGitExtension.ps1
-. $PSScriptRoot\Use-Git.ps1
-. $PSScriptRoot\Out-Git.ps1
+$CommandsPath = Join-Path $PSScriptRoot Commands
+:ToIncludeFiles foreach ($file in (Get-ChildItem -Path "$CommandsPath" -Filter "*-*.ps1" -Recurse)) {
+    if ($file.Extension -ne '.ps1')      { continue }  # Skip if the extension is not .ps1
+    foreach ($exclusion in '\.[^\.]+\.ps1$') {
+        if (-not $exclusion) { continue }
+        if ($file.Name -match $exclusion) {
+            continue ToIncludeFiles  # Skip excluded files
+        }
+    }     
+    . $file.FullName
+}
