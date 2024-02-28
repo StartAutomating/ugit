f16c40f064a1504da6caa36b6280d0195625d365
James Brundage <@github.com>
2024-02-28 10:56:52 -0800

    chore: Moving Get-UGitExtension into /Commands ( Fixes #243 )

--- ugit.psm1
+++ ugit.psm1
@@ -1,3 +1,4 @@
-﻿. $psScriptRoot\Get-UGitExtension.ps1
+﻿$commandsPath = Join-Path $PSScriptRoot Commands
+. $commandsPath\Get-UGitExtension.ps1
 . $PSScriptRoot\Use-Git.ps1
 . $PSScriptRoot\Out-Git.ps1
