8e7d999a791d254d64a4ffec45005aeaac938aa3
James Brundage <@github.com>
2024-02-28 11:01:40 -0800

    chore: Moving Piecemeal into /Build ( Fixes #241 )
    Simplifying Paths

--- Build/ugit.Piecemeal.ps1
+++ Build/ugit.Piecemeal.ps1
@@ -1,12 +1,6 @@
 #require -Module Piecemeal
-Push-Location ($PSScriptRoot | Split-Path)
+Push-Location ($PSScriptRoot | Split-Path | Join-Path -ChildPath "Commands")
 
-$commandsPath = Join-Path $pwd "Commands"
-
-if (-not (Test-Path $commandsPath)) {
-    New-Item -ItemType Directory -path $commandsPath | Out-Null
-}
-$outputPath = Join-Path $commandsPath 'Get-UGitExtension.ps1'
-Install-Piecemeal -ExtensionModule 'ugit' -ExtensionModuleAlias 'git' -ExtensionNoun 'UGitExtension' -ExtensionTypeName 'ugit.extension' -OutputPath $outputPath
+Install-Piecemeal -ExtensionModule 'ugit' -ExtensionModuleAlias 'git' -ExtensionNoun 'UGitExtension' -ExtensionTypeName 'ugit.extension' -OutputPath '.\Get-UGitExtension.ps1'
 
 Pop-Location
