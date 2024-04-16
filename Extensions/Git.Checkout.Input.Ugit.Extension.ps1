2fa775ea96e388e8109597b1e3c39a1e8f52c9b1
James Brundage <@github.com>
2024-04-15 21:23:50 -0700

    feat: git checkout -PullRequest ( Fixes #178 )

---
+++ Extensions/Git.Checkout.Input.Ugit.Extension.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    git checkout input extension
+.DESCRIPTION
+    Extends the parameters for git checkout, making it easier to use from PowerShell.
+
+    If the -PullRequest parameter is provided, the branch will be fetched from the remote.
+
+    If the -BranchName parameter is provided, the branch will be checked out.
+#>
+[ValidatePattern('^git checkout')]
+[Management.Automation.Cmdlet("Use","Git")]
+[CmdletBinding(PositionalBinding=$false)]
+param(
+# The branch name.
+[Parameter(ValueFromPipelineByPropertyName)]
+[string]
+$BranchName,
+
+# The number of the pull request.
+# If no Branch Name is provided, the branch will be `PR-$PullRequest`.
+[int]
+$PullRequest
+)
+
+if ($PullRequest) {
+    $remoteName = git remote | Select-Object -ExpandProperty RemoteName | Select-Object -First 1
+    if (-not $BranchName) {
+        $BranchName = "PR-$PullRequest"        
+    }
+    $fetchedBranch = git fetch $remoteName "pull/$PullRequest/head:$BranchName"
+    if (-not $fetchedBranch) { 
+        return $BranchName
+    }
+    $BranchName
+} elseif ($BranchName) {    
+    $BranchName
+}
