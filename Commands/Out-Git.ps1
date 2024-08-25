5a6d021d3bb7d29889339c3847f13c3c49a57c57
James Brundage <@github.com>
2024-08-25 11:02:05 -0700

    fix: git clone -Since improvements ( Fixes #276 )
    Improving Co-authored-by tag (using no-reply email)
    
    Co-authored-by: Jake Bolton <3892031+ninmonkey@users.noreply.github.com>
    Co-authored-by: James Brundage <1043665+startautomating@users.noreply.github.com>

--- Commands/Out-Git.ps1
+++ Commands/Out-Git.ps1
@@ -193,7 +193,7 @@
             if ("$out" -match "^(?:error|fatal):") {
                 $exception = [Exception]::new($("$out" -replace '^(?:error|fatal):')) # Create an exception
                 # Clean up --shallow-since exceptions, since git gives less than obvious error messages in this scenario.
-                # Hat tip @ninmonkey for the scenario!
+                # Hat tip @ninmonkey for the scenario! (see #276)
                 if ($exception -match 'shallow info: \d+\s{0,}$' -and $gitCommand -match '--shallow-since=(?<shallowdate>\S+)') {
                     $exception = [Exception]::new("No commits found -Since $($matches.shallowdate)")
                 }
