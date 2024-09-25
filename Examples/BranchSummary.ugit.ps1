d50063133a928bc3ac454b2c3e3dd0163abbfbc3
James Brundage <+@noreply.github.com>
2024-09-24 21:10:20 -0700

    docs: BranchSummary ugit action example ( Fixes #291 )

---
+++ Examples/BranchSummary.ugit.ps1
@@ -0,0 +1,36 @@
+<#
+.SYNOPSIS
+    Generates a summary of the current branch
+.DESCRIPTION
+    This will generate a summary of the current branch, in markdown format.
+#>
+param()
+
+$currentBranchName = git branch | ? IsCurrentBranch
+if ($currentBranchName -in 'master','main') {
+    Write-Warning "Not summarizing the main branch."    
+    return
+}
+
+$currentBranchCommits = git log -CurrentBranch -Statistics 
+$currentBranchCommits | Out-Host
+$markdownTable = '|GitUserName|CommitDate|CommitMessage|'
+'|-|:-:|-|'
+foreach ($gitlog in $currentBranchCommits) {    
+    '|' + $(
+        $gitLog.GitUserName, 
+        $gitLog.GitCommitDate.ToString(), 
+        ($gitLog.CommitMessage -replace '(?>\r\n|\n)', '<br/>') -join '|'
+    ) + '|'
+}
+
+if ($env:GITHUB_STEP_SUMMARY) {
+    $remoteUrl = git remote | git remote get-url
+    "
+## branch summary
+
+$markdownTable
+" |
+        Out-File -Append -FilePath $env:GITHUB_STEP_SUMMARY
+}
+
