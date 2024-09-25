5019296973d622d93342eb58baa084f5114507e9
James Brundage <+@noreply.github.com>
2024-09-24 22:36:23 -0700

    docs: ChangesByDayOfWeek example ( Fixes #295 )

---
+++ Examples/ChangesByDayOfWeek.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+    Generates a Mermaid graph of changes by day of week.
+.DESCRIPTION
+    Generates a Mermaid graph of changes by day of week.
+#>
+param()
+
+$gitRemote = git remote
+$headBranch = git remote |
+    Select-Object -First 1 |
+    git remote show |
+    Select-Object -ExpandProperty HeadBranch
+
+$currentBranch = git branch | ? IsCurrentBranch
+if ($currentBranchName -eq $headBranch) {
+    Write-Warning "Not graphing the main branch."
+    return
+}
+
+$commitList = 
+    if ($currentBranch -ne $headBranch) {
+        git log "$($gitRemote.RemoteName)/$headBranch..$CurrentBranch"
+    } else {
+        git log 
+    }
+
+$groupedChangedSet = $currentBranchCommits |     
+    Group-Object { $_.CommitDate.DayOfWeek } -NoElement
+
+if ($env:GITHUB_STEP_SUMMARY) { 
+"
+~~~mermaid
+$(
+@(
+"pie title Changes by Day Of Week"
+foreach ($changeSet in $groupedChangedSet) {
+    (' ' * 4) + '"' + $($changeSet.Name) + '"' + ' : ' + ($changeSet.Count)
+}
+) -join [Environment]::NewLine)
+~~~
+
+" |
+        Out-File -Append -FilePath $env:GITHUB_STEP_SUMMARY
+}
+
