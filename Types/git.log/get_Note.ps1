caf8c96dd8ceb7de3dab3d8c6ecb9b25dda2c481
James Brundage <+@noreply.github.com>
2024-09-24 22:59:34 -0700

    feat: git.log.note(s) ( Fixes #296 )
    git notes show

--- Types/git.log/get_Note.ps1
+++ Types/git.log/get_Note.ps1
@@ -2,5 +2,8 @@
 .SYNOPSIS
     Gets git commit notes
 .DESCRIPTION
-    Gets git notes associated with a commit.
+    Gets git notes associated with a commit.    
 #>
+Push-Location $this.GitRoot
+git notes show $this.CommitHash
+Pop-Location
