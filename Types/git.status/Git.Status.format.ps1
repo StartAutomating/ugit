0159dbf638c30c398b32d68de2dd51cc1eebc3f4
James Brundage <+@noreply.github.com>
2026-07-21 10:32:18 -0700

    feat: `git status` formatting improvment ( Fixes #363 )
    Adjusting colors and spacing

--- Types/git.status/Git.Status.format.ps1
+++ Types/git.status/Git.Status.format.ps1
@@ -1,45 +1,113 @@
 Write-FormatView -TypeName Git.Status -Action {
     Write-FormatViewExpression -Text "On branch: "
-    Write-FormatViewExpression -ScriptBlock { $_.BranchName } -if { $_.BranchName -notin 'main', 'master' } -ForegroundColor Verbose
-    Write-FormatViewExpression -ScriptBlock { $_.BranchName } -if { $_.BranchName -in 'main', 'master' } -ForegroundColor Warning
+
+    Write-FormatViewExpression -ScriptBlock { 
+        $branchName = $_.BranchName
+        @(
+            if ($branchName -notin 'main', 'master', 'latest') {
+                $PSStyle.Foreground.Cyan
+                $PSStyle.Bold
+            } else {
+                $PSStyle.Formatting.Warning
+            }
+            $branchName
+            $PSStyle.Reset
+        ) -join ''                
+    }
     Write-FormatViewExpression -Newline
-    Write-FormatViewExpression -If { $_.Status -notlike 'Nothing*' } -ScriptBlock { $_.Status + [Environment]::NewLine }
+    
+    Write-FormatViewExpression -ScriptBlock {
+        $gitStatus = $_
+        if ($gitStatus.Status -match 'nothing') {
+            return ''
+        }
+        @(
+            if ($gitStatus.Ahead) {
+                $PSStyle.Formatting.Success
+            } elseif ($gitStatus.Behind) {
+                $PSStyle.Formatting.Error
+            }
+            else {
+                $PSStyle.Foreground.Cyan
+            }
+            $gitStatus.Status + [Environment]::NewLine
+            $PSStyle.Reset
+        ) -join ''        
+    }    
+
     Write-formatviewExpression -If { $_.Staged } -ScriptBlock {
         "Changes Staged For Commit:
   (use git commit -m to commit)" + [Environment]::NewLine
     }
-    Write-FormatViewExpression -If { $_.Staged } -ScriptBlock {
-        (@(foreach ($line in $($_.Staged | Select-Object ChangeType, Path | Out-String -Width ($host.UI.RawUI.BufferSize.Width - 8)) -split '(?>\r\n|\n)') {
-            (" " * 4) + $line
-        }) -join [Environment]::NewLine) + [Environment]::NewLine
-    } -ForegroundColor Green
 
-    Write-FormatViewExpression -If { $_.Unstaged } -ScriptBlock {
-        "Changes Not Staged For Commit:
-  (use git add <file> to add, git restore <file> to discard changes)" + [Environment]::NewLine
+    Write-FormatViewExpression -If { $_.Staged } -ScriptBlock {
+        (@(
+            foreach ($staged in $_.Staged) {                
+                "`t$($staged.ChangeType):`t$($staged.Path)"
+            }    
+        ) -join [Environment]::NewLine) + (
+            [Environment]::NewLine * 2
+        )
     }
 
-    Write-FormatViewExpression -If { $_.Unstaged}   -ScriptBlock {
-        (@(foreach ($line in $($_.Unstaged | Select-Object ChangeType, Path | Out-String -Width ($host.UI.RawUI.BufferSize.Width - 8)) -split '(?>\r\n|\n)') {
-            (" " * 4) + $line
-        }) -join [Environment]::NewLine) + [Environment]::NewLine
-    } -ForegroundColor Red
+    Write-FormatViewExpression -If { $_.Unstaged } -ScriptBlock {
+        (@(
+            "Changes $($PSStyle.Foreground.Cyan + $PSStyle.Bold)Not Staged$($PSStyle.Reset) for commit:"
+            '  (use "git add <file>..." to update what will be committed)'
+            '  (use "git restore <file>..." to discard changes in working directory)'
+        ) -join [Environment]::NewLine) + (
+            [Environment]::NewLine * 2
+        )
+    }
+
+    Write-FormatViewExpression -If { $_.Unstaged }   -ScriptBlock {
+        (@(
+            foreach ($staged in $_.Unstaged) {                
+                "`t$(
+                    if ($staged.changetype -eq 'modified') {
+                        $PSStyle.Foreground.Cyan
+                    } 
+                    elseif ($PSStyle.changetype -eq 'newfile') {
+                        $PSStyle.Foreground.Green
+                    }
+                    elseif ($staged.changetype -eq 'deleted') {
+                        $PSStyle.Foreground.Red
+                    } else {
+                        $PSStyle.Foreground.White
+                    }
+                    $psStyle.Bold                    
+                )$($staged.ChangeType):`t$($staged.Path)$($PSStyle.Reset)"
+            }    
+        ) -join [Environment]::NewLine) + (
+            [Environment]::NewLine * 2
+        )
+    }
 
     Write-FormatViewExpression -If { $_.Untracked } -ScriptBlock {
-        "Untracked Files:
-  (use git add <file> to include in commit)" + [Environment]::NewLine
+        (@(
+            "$($PSStyle.Foreground.Magenta + $PSStyle.Bold)Untracked$($PSStyle.Reset) Files:"
+            '  (use "git add <file>..." to include in what will be committed)'
+        ) -join [Environment]::NewLine) + (
+            [Environment]::NewLine * 2
+        )
     }
 
     Write-FormatViewExpression -If { $_.Untracked}   -ScriptBlock {
-        @(foreach ($line in $($_.Untracked | Out-String -Width ($host.UI.RawUI.BufferSize.Width - 8)) -split '(?>\r\n|\n)') {
-            (" " * 4) + $line
-        }) -join [Environment]::NewLine
-    } -ForegroundColor Yellow
+        (@(
+            $gitRoot = $_.GitRoot
+            foreach ($untracked in $_.Untracked) {
+                "`t$(
+                    $untracked.FullName.Substring($gitRoot.Length + 1) -replace '[\\/]', '/'
+                )"
+            }            
+        ) -join [Environment]::NewLine) + 
+            [Environment]::NewLine + [Environment]::NewLine
+    } -ForegroundColor Magenta
 
     Write-FormatViewExpression -ScriptBlock {
         if(($_.Untracked.Count + $_.Unstaged.Count + $_.Staged.Count) -eq 0)
         {
             "Nothing to commit, working tree clean"
         }
-    }
+    } -ForegroundColor Success
 }
